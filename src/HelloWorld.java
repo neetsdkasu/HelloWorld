ab08765afa67b874d368a42528b18f0ad51d9c4f
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2013-10-12 00:49:06 +0900

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,11 @@
 
 class HelloWorld
 {
+  public static void main(String[] args) {
+    HelloWorld hw = new HelloWorld("Hello World!");
+    hw.foo();
+  }
+  
   private final String message;
   
   HelloWorld(String message) {
@@ -13,10 +18,5 @@ class HelloWorld
   
   void foo() {
     System.out.println(this.message);
-  }
-  
-  public static void main(String[] args) {
-    HelloWorld hw = new HelloWorld("Hello World!");
-    hw.foo();
   }
 }

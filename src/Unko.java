9eef72a31811825249572dd8d066b82671938d58
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2013-11-02 17:55:45 +0900

    Create Unko.java

---
+++ src/Unko.java
@@ -0,0 +1,37 @@
+
+
+public class Unko
+{
+  private final String name;
+  private final int value;
+  
+  public Unko(String name, int value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getName() { return name; }
+  public int getValue() { return value; }
+  
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash ^= (name == null ? 0 : name.hashCode());
+    hash ^= value;
+    return hash ^ super.hashCode();
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (this.getClass() != obj.getClase())  return false;
+    Unko other = (Unko)obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    if (value != other.value) return false;
+    return true;
+  }
+
+}

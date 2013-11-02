b60eb74550e578004094dfb2e06e794509a319b7
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2013-11-02 17:57:06 +0900

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,15 @@ class HelloWorld
  	public static void main(String[] args) {
       		HelloWorld hw = new HelloWorld("Hello World!");
           	hw.foo();
+          	
+          	
+          	Unko unko = new Unko("Unko dayo", 100);
+          	Unko kuso = new Unko("Unko dayo", 100);
+          	System.out.println(unko);
+          	System.out.println(kuso);
+          	System.out.println(unko.equals(kuso));
+          	
+          	
 	}
 	      
 	private final String message;
